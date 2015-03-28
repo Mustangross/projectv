08a1f8cd5701f67d11d81e12698c459781cd096f
Mustangross <andrepgross@gmail.com>
2015-03-28 14:41:29 -0700

    Saved scene and it's bugged. Probably using 3d code for a 2d rigid body :P

---
+++ projectv/Assets/Player/Player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public float f_Speed = 10f;
+	public float f_RunSpeed = 15f;
+	public Vector3 teleportPoint;
+	public Rigidbody rb_RigidBody;
+
+	//Check this line of code in the future.
+	//move.rigidbody.velocity = transform.TransformDirection(Vector3.left * f_RunSpeed);
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb_RigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Gets Left Arrow Input to move left
+		if(Input.GetKey(KeyCode.LeftArrow) == true)
+		{
+			//moves the Player to the left using the basic speed
+			rb_RigidBody.MovePosition(transform.position + transform.left * Time.deltaTime * f_Speed);
+
+			//Ensure the Speed provided is the Run Speed and not the normal speed
+			if(Input.GetKey(KeyCode.LeftShift)) // && Input.GetKey(KeyCode.LeftArrow) == true))
+			{
+				rb_RigidBody.MovePosition (transform.position + transform.left * Time.deltaTime * f_RunSpeed);
+			}
+		}
+
+		//Gets Left Arrow Input to move right
+		if(Input.GetKey(KeyCode.RighttArrow) == true)
+		{
+			//moves the Player to the left using the basic speed
+			rb_RigidBody.MovePosition = (transform.position + transform.right * Time.deltaTime * f_Speed);
+			
+			//Ensure the Speed provided is the Run Speed and not the normal speed
+			if(Input.GetKey(KeyCode.LeftShift)) // && Input.GetKey(KeyCode.LeftArrow) == true))
+			{
+				rb_RigidBody.MovePosition (transform.position + transform.right * Time.deltaTime * f_RunSpeed);
+			}
+		}
+	}
+}
