683e9ac19fb5a46d1a4c307479b8803a9d61464b
Eric <nacchii@gmail.com>
2015-03-28 15:53:28 -0700

    Ignore some files. Improve enemy behaviour.

--- projectv/Assets/Scripts/EnemyController.cs
+++ projectv/Assets/Scripts/EnemyController.cs
@@ -8,23 +8,38 @@ public class EnemyController : MonoBehaviour
 	public int initial_movement_direction;
 	public float movement_speed;
 	public float travel_distance;
+	private float pause_countdown;
+	public float pause_interval;
 
 	// Use this for initialization
 	void Start ()
 	{
 		movement_direction = initial_movement_direction;
 		transform.position = spawn_position;
+		pause_countdown = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		Vector3 pos = transform.position;
-		pos.x += movement_direction * movement_speed;
-		transform.position = pos;
-		if (pos.x >= spawn_position.x + travel_distance || pos.x <= spawn_position.x)
+		if (pause_countdown > 0)
 		{
-			movement_direction = -movement_direction;
+			pause_countdown -= Time.deltaTime;
 		}
+		else
+		{
+			Vector3 pos = transform.position;
+			pos.x += movement_direction * movement_speed;
+			transform.position = pos;
+			if (pos.x >= spawn_position.x + travel_distance || pos.x <= spawn_position.x) {
+				movement_direction = -movement_direction;
+				pause_countdown = pause_interval;
+			}
+		}
+	}
+
+	void OnTriggerEnter(GameObject other)
+	{
+		Destroy (gameObject);
 	}
 }
